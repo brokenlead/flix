b46e7dea30a1878eb791afc0702a0c1a7bb52675
Jimmy Compher <jimmy.compher@gmail.com>
2014-10-04 18:18:05 -0400

    Initial flix app

---
+++ app/helpers/movies_helper.rb
@@ -0,0 +1,32 @@
+module MoviesHelper
+  def format_total_gross(movie)
+    if movie.flop?
+      content_tag(:strong, 'Flop!')
+    else
+     number_to_currency(movie.total_gross)
+    end
+  end
+
+  def image_for(movie)
+    if movie.image_file_name.blank?
+     image_tag('placeholder.png')
+    else
+     image_tag(movie.image_file_name)
+    end
+  end
+
+  def format_average_stars_for(movie)
+    if movie.reviews.count > 0
+      link_to("Read #{pluralize(movie.reviews.count, 'Review')}", movie_reviews_path(movie)) +
+      " (Average " +
+        stars_average_for(movie) +
+        ")"
+    else
+      link_to "No Reviews", movie_reviews_path(movie)
+    end
+  end
+
+  def stars_average_for(movie)
+    content_tag(:strong, pluralize(number_with_precision(movie.average, precision: 1), "star"))
+  end
+end
---
+++ db/migrate/20141001020659_add_location_to_reviews.rb
@@ -0,0 +1,6 @@
+class AddLocationToReviews < ActiveRecord::Migration
+  def change
+    add_column :reviews, :city, :string
+    add_column :reviews, :state, :string
+  end
+end
