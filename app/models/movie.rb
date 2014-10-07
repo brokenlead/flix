4ee8d4e65f7ef7b4ba9189b2f93e9422da8f5421
Jimmy Compher <jimmy.compher@gmail.com>
2014-10-06 21:49:01 -0400

    Implement Paperclip for image uploads

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,12 +2,17 @@ class Movie < ActiveRecord::Base
 
   has_many :reviews, dependent: :destroy
 
+  has_attached_file :image
+
   RATINGS = %w(G PG PG-13 R NC-17)
 
   validates :title, :released_on, :duration, presence: true
   validates :description, length: {minimum: 25}
   validates :total_gross, numericality: {greater_than_or_equal_to: 0}
   validates :rating, inclusion: {in: RATINGS}
+  validates_attachment :image,
+    :content_type => { :content_type => ['image/jpeg', 'image/png']},
+    :size => { :less_than => 1.megabyte}
 
   def self.released
     where("released_on <= ?", Time.now).order("released_on desc")
