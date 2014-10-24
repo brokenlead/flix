159882f072e1d466e21a1a91b84a19a53257880e
Jimmy Compher <jimmy.compher@gmail.com>
2014-10-23 20:03:31 -0400

    Restrict access to accounts

--- spec/features/list_users_spec.rb
+++ spec/features/list_users_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe 'Viewing the list of users' do
     it 'shows the list of users' do
-        user1 = User.create!(user_attributes(name:'user1', email:'user1@test.com'))
-        user2 = User.create!(user_attributes(name:'user2', email:'user2@test.com'))
-        user3 = User.create!(user_attributes(name:'user3', email:'user3@test.com'))
+        user1 = User.create!(user_attributes(name:'user1', username:'user1', email:'user1@test.com'))
+        user2 = User.create!(user_attributes(name:'user2', username:'user2', email:'user2@test.com'))
+        user3 = User.create!(user_attributes(name:'user3', username:'user3', email:'user3@test.com'))
+        sign_in(user1)
         visit users_url
         expect(page).to have_link(user1.name)
         expect(page).to have_link(user2.name)
--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -4,6 +4,7 @@ describe "Viewing a user" do
 
     it "shows the user information" do
         user = User.create!(user_attributes)
+        sign_in(user)
         visit user_url(user)
         expect(page).to have_text(user.name)
         expect(page).to have_text(user.email)
